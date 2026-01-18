b6113c6122bcd811a1b1f340c3823477579e2e80
Sakthi Shree <sakthishree0302@gmail.com>
2026-01-18 15:45:46 +0530

    Add JWT login endpoints

--- backend/talentlink_backend/urls.py
+++ backend/talentlink_backend/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/login/', TokenObtainPairView.as_view()),
-    path('api/refresh/', TokenRefreshView.as_view()),
-
-    path('api/users/', include('users.urls')),
+    # JWT Authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
